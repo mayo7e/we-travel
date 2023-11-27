998371e3bcf863273b987e0daa48bcfe997b7234
mayo7e <mayowa.wh@gmail.com>
2023-11-27 06:53:47 +0100

    list section

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,11 +1,56 @@
 
 
-import React from 'react'
+import React, {useState} from 'react'
+
+const Places= [
+  {name :"Neo Cafe"},
+  {name :"Connect Central"},
+  {name :"Creative Collective"},
+  {name :"Conclave Corner"},
+  {name :"Sip & Gather"},
+  {name :"Fusion Chambers"}
+]
+
 
 const List = () => {
+
+  const [workHubs, setWorkHubs] = useState("Cafes")
+  const [ratings, setRatings] = useState("")
+
   return (
     <div>
-      <h1>List</h1>
+      <h4>Cafes, Workspaces and Meeting rooms</h4>
+          <ul>
+            <li>
+              <label htmlFor='filterDropdown'>Filter</label>
+              <br />  <br />
+                  <select value={workHubs} id='filterDropdown' onChange={(e)=>setWorkHubs(e.target.value)} >
+                    {console.log(workHubs)}
+                    <option value="Cafes">Cafes</option>
+                    <option value="WorkSpaces">Workspaces</option>
+                    <option value="Meetings" >Meeting rooms</option>
+                  </select>
+            </li>
+            <br />
+            <li>
+              <label htmlFor='filterDropdown'>fRatings</label>
+              <br />  <br />
+                  <select value={ratings} id='filterDropdown' onChange={(e)=>setRatings(e.target.value)} >
+                    {console.log(ratings)}
+                    <option value={0}>All</option>
+                    <option value={3.5}>Above 3.5</option>
+                    <option value={4.0} >Above 4.0</option>
+                    <option value={4.5} >Above 4.5</option>
+                  </select>
+            </li>
+          </ul>
+          <div>
+              {Places.map((place)=>(
+                <div>
+                  <p>{place.name}</p>
+                </div>
+              ))}
+          </div>
     </div>
   )
 }
